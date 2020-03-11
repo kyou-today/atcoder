5c5b8f6dcd3020fa3ddb78e1cb7ad98e72223329
kyou-today <kyoutoday@kyou-today.dev>
2020-03-12 02:22:26 +0900

    [WA] ABC158-C (https://atcoder.jp/contests/abc158/submissions/10765264)

---
+++ src/abc158/CTaxIncrease.kt
@@ -0,0 +1,20 @@
+package abc158
+
+import intList
+import java.lang.Math.floor
+
+fun main(args: Array<String>) {
+    val (A, B) = intList().map(Int::toDouble)
+    // v * 0.08 = A
+    // v * 0.10 = B
+
+    for(i in 1..100) {
+        val c = floor(i * 0.08)
+        val d = floor(i * 0.10)
+        if(A == c && B == d) {
+            println(i)
+            return
+        }
+    }
+    println(-1)
+}
