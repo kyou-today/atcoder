fbd77684d9b638bc17dcb240d258f42e2181e0e6
kyou-today <kyoutoday@kyou-today.dev>
2020-03-12 01:53:51 +0900

    [AC] ABC158-B (https://atcoder.jp/contests/abc158/submissions/10764950)

--- src/abc158/BCountBalls.kt
+++ src/abc158/BCountBalls.kt
@@ -1,24 +1,12 @@
 package abc158
 
 import longList
+import java.lang.Math.min
 
 fun main(args: Array<String>) {
     val (N, A, B) = longList()
 
-    val t = N % (A + B)
-    val c = N / (A + B)
-
-    if(t == 0L) {
-        if(A + B > N) {
-            println(N - A)
-        }else{
-            println(A * c)
-        }
-    }else{
-        if(t > A) {
-            println(A * c + (t - A))
-        }else{
-            println(A * c + t)
-        }
-    }
+    val res = N / (A + B) * A
+    val c = N % (A + B)
+    println(res + min(c, A))
 }
