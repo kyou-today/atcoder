242bd7c92b3822cd230fabd6d16c24f97a344e6d
mirror-kt <me@mirror-kt.dev>
2023-06-08 13:14:00 +0900

    ✅ Submit ABC304-A
    https://atcoder.jp/contests/abc304/submissions/42070407

--- abc304/src/bin/a.rs
+++ abc304/src/bin/a.rs
@@ -3,6 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        sa: [(String, u32); n],
+    }
+
+    let mut min_index = 0;
+    let mut min_age = sa[0].1;
+
+    for i in 0..n {
+        if sa[i].1 < min_age {
+            min_age = sa[i].1;
+            min_index = i;
+        }
+    }
+
+    for i in 0..n {
+        println!("{}", sa[(min_index + i) % n].0)
     }
 }
