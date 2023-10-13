35fca31bf1623d25b2ccd91561235c8ccd8b8cf6
mirror-kt <me@mirror-kt.dev>
2023-10-13 19:40:44 +0900

    ✅ Submit ABC322-B
    https://atcoder.jp/contests/abc322/submissions/46493964

--- abc322/src/bin/b.rs
+++ abc322/src/bin/b.rs
@@ -3,7 +3,18 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        _n: u32, _m: u32,
+        s: String,
+        t: String,
+    }
+
+    if t.starts_with(&s) && t.ends_with(&s) {
+        println!("0");
+    } else if t.starts_with(&s) {
+        println!("1");
+    } else if t.ends_with(&s) {
+        println!("2");
+    } else {
+        println!("3");
     }
 }
